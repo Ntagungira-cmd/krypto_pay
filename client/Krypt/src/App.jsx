10325a41c49b8e843584c91f4c609cda1f3ca571
ntagungira-cmd <ntagungiraali@gmail.com>
2022-10-05 10:05:43 +0200

    initial commit

---
+++ client/Krypt/src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar, Welcome, Loader, Footer, Services, Transactions } from "./components"
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App

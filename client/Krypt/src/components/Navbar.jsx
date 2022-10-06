2cbf65957aa8f2d8988b8a8cb0ac70f4dda2f937
ntagungira-cmd <ntagungiraali@gmail.com>
2022-10-06 14:51:08 +0200

    add logoa nd icons

--- client/Krypt/src/components/Navbar.jsx
+++ client/Krypt/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    
+        </nav>
     )
 }
 

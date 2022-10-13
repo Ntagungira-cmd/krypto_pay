82505449a2a2f2658d4ae3e271ec982b699ef043
ntagungira-cmd <ntagungiraali@gmail.com>
2022-10-13 22:01:37 +0200

    inne

--- client/Krypt/src/components/Welcome.jsx
+++ client/Krypt/src/components/Welcome.jsx
@@ -31,7 +31,7 @@ const Welcome = () => {
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                        Send Crypto <br /> across the globe
+                        Send Crypto <br /> Across The Globe
                     </h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.

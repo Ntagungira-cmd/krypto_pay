e0651a684aa350517ee76d808629d8c993c9f897
ntagungira-cmd <ntagungiraali@gmail.com>
2022-11-15 14:24:29 +0200

    add transactions smart contracts

---
+++ smart-contracts/contracts/Transaction.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+contract Transactions {
+
+    uint256 transactionCount;
+
+    event transfer(address from , address reciever , uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct {
+        address from;
+        address reciever;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable reciever, uint amount, string memory message, string memory keyword) public {
+        transactionCount+=1;
+        transactions.push(TransferStruct(msg.sender, reciever, amount, message, block.timestamp, keyword));
+
+        emit transfer(msg.sender, reciever, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransfers() public view returns (TransferStruct[] memory) {
+        //return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+}

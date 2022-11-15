c405a0b75ddf24a8b0472a3278aa3623da142d7c
ntagungira-cmd <ntagungiraali@gmail.com>
2022-11-15 21:39:33 +0200

    deploy transaction contract

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,23 @@
+//https://eth-goerli.g.alchemy.com/v2/B-3yptzYVsEw0y9SinxPdjjvckL4pxhG
+
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "https://eth-goerli.g.alchemy.com/v2/B-3yptzYVsEw0y9SinxPdjjvckL4pxhG";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "ab5c6b2bce15423e45188f66b485fcadcc4df975fb82379637e2c355d460a4b4";
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
